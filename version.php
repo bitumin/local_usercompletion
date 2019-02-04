48c072c96b36d06ad3136452c2ae55fc51c714ff
Mitxel Moriana <mitxel@tresipunt.com>
2019-02-04 12:54:10 +0100

    increase version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_usercompletion';
-$plugin->version = 2019020402;
+$plugin->version = 2019020403;
 $plugin->release = '0.0.2';
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
